e73191f634c441157e87f936e0860f89c70ee64f
Atul Varma <varmaa@gmail.com>
2014-08-12 10:13:19 -0400

    simplify export script.

--- bin/export.js
+++ bin/export.js
@@ -5,9 +5,7 @@ var fs = require('fs');
 var BASE_URL = process.argv[2] ||
                'http://toolness.github.io/teeny-quiz-fun/';
 
-var content = fs.readFileSync(__dirname + '/../index.html', 'utf-8');
-
-content = content
+var content = fs.readFileSync(__dirname + '/../index.html', 'utf-8')
   .replace('href="teeny-quiz.css"',
            'href="' + BASE_URL + 'teeny-quiz.css"')
   .replace('src="teeny-quiz.js"',
